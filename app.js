5cf283d2c89261175baf44bb293909ddc2061f91
muhammadayaz354 <160526605+muhammadayaz354@users.noreply.github.com>
2024-02-26 19:07:52 +0500

    Exersice no 4
    Famous quote

---
+++ app.js
@@ -0,0 +1,3 @@
+var author = "Thomas A. Edison";
+var quote = "Tomorrow is my exam but i don't care because a single sheet of paper cannot decide my future";
+console.log("".concat(author, " Once Said, ").concat(quote));
