f1d093598a971b060044e7a9c4e5901eaec22ecc
muhammadayaz354 <160526605+muhammadayaz354@users.noreply.github.com>
2024-02-26 19:09:30 +0500

    Exersice no 5
    Famous quote 2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
-var author = "Thomas A. Edison";
+var famousperson = "Thomas A. Edison";
 var quote = "Tomorrow is my exam but i don't care because a single sheet of paper cannot decide my future";
-console.log("".concat(author, " Once Said, ").concat(quote));
+var message = "".concat(famousperson, " Once Said, ").concat(quote);
+console.log(message);
