f9fa8697e53b2298c74cb4ce466d4cd1102baf42
muhammadayaz354 <160526605+muhammadayaz354@users.noreply.github.com>
2024-02-26 19:17:05 +0500

    Exersice no 10
    Adding Comments

---
+++ program.js
@@ -0,0 +1,5 @@
+"use strict";
+//This is Ayaz;
+//Todat date is 2/22/2024;
+// This Program will be print a Famous quote:
+console.log("Tomorrow is my exam but i don't care because a single sheet of paper cannot decide my future");
---
+++ program2.js
@@ -0,0 +1,5 @@
+"use strict";
+//This is Ayaz;
+//Todat date is 2/22/2024;
+// This Program will be Adding numbers:
+console.log(5 + 6);
